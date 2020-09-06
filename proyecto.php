f23bcbb0e75bcfc0409b5ff5cd25682972782553
David Palacios <paladavid@hotmail.com>
2020-09-05 21:35:55 -0500

    Comentarios corregidos

--- proyecto.php
+++ proyecto.php
@@ -107,20 +107,20 @@ if (array_key_exists('ciudad', $_GET)) {
         <div id="map" class="map"></div>
         <script type="text/javascript">
             function initMap() {
-                // The location of Uluru
-                var uluru = {
+                // The location of the city
+                var city = {
                     lat: <?php echo $latitud?>,
                     lng: <?php echo $longitud?>
                 };
-                // The map, centered at Uluru
+                // The map, centered at City
                 var map = new google.maps.Map(
                     document.getElementById('map'), {
                         zoom: 12,
-                        center: uluru
+                        center: city
                     });
-                // The marker, positioned at Uluru
+                // The marker, positioned at City
                 var marker = new google.maps.Marker({
-                    position: uluru,
+                    position: city,
                     map: map
                 });
             }
@@ -132,9 +132,8 @@ if (array_key_exists('ciudad', $_GET)) {
     <script defer src="https://maps.googleapis.com/maps/api/js?key=APIKEY&callback=initMap"> </script>
 
     <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- jQuery first, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
 </body>
 
